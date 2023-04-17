2b5377756df5f83ebf5611fb89c5e5b3e9fb678f
iSplasher <isplasher@gmail.com>
2023-04-17 03:39:01 +0200

    update

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,5 +1,26 @@
 return {
 
+    -- Smooth scroll
+    {
+        'karb94/neoscroll.nvim',
+        config = function()
+            require('neoscroll').setup({
+                pre_hook = function()
+                    vim.opt.eventignore:append({
+                        'WinScrolled',
+                        'CursorMoved',
+                    })
+                end,
+                post_hook = function()
+                    vim.opt.eventignore:remove({
+                        'WinScrolled',
+                        'CursorMoved',
+                    })
+                end,
+            })
+        end
+    },
+
     -- Add indentation guides even on blank lines
     {
         'lukas-reineke/indent-blankline.nvim',
